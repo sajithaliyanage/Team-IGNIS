164bb128cab79f2190b56c305b47704004c75dde
Sajitha Liyanage <sajithaliyanage@gmail.com>
2016-04-15 17:31:55 +0530

    change

--- index.php
+++ index.php
@@ -8,31 +8,32 @@
 </head>
 <body>
 
-<div class="container top-down">
-    <div class="row" style="margin-top:20px">
-        <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+<div class="container ">
+    <div class="row" >
+        <div id="login-box" class="col-xs-10 col-xs-offset-1 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
             <form role="form">
                 <fieldset>
-                    <h2>Please Sign In</h2>
+                    <h2>SignIn to Take Your Leave</h2>
                     <hr class="colorgraph">
                     <div class="form-group">
-                        <input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address">
+                        <input type="text" name="email" id="email" class="form-control input-lg" placeholder="Company ID">
                     </div>
                     <div class="form-group">
                         <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password">
                     </div>
-				<span class="button-checkbox">
-					<button type="button" class="btn" data-color="info">Remember Me</button>
-                    <input type="checkbox" name="remember_me" id="remember_me" checked="checked" class="hidden">
-					<a href="" class="btn btn-link pull-right">Forgot Password?</a>
-				</span>
-                    <hr class="colorgraph">
+
+                    <a href="" class="btn btn-link pull-right">Forgot Password?</a>
+
+                    <hr class="colorgraph" style="margin-top:60px;">
                     <div class="row">
+                        <div class="col-xs-3 col-sm-3 col-md-3">
+
+                        </div>
                         <div class="col-xs-6 col-sm-6 col-md-6">
                             <input type="submit" class="btn btn-lg btn-success btn-block" value="Sign In">
                         </div>
-                        <div class="col-xs-6 col-sm-6 col-md-6">
-                            <a href="" class="btn btn-lg btn-primary btn-block">Register</a>
+                        <div class="col-xs-3 col-sm-3 col-md-3">
+
                         </div>
                     </div>
                 </fieldset>
