167df198d47bdd4236d7add24efab8c0919f5e65
Sajitha Liyanage <sajithaliyanage@gmail.com>
2016-04-27 11:55:00 +0530

    updated

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <div class="container ">
     <div class="row" >
         <div id="login-box" class="col-xs-10 col-xs-offset-1 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
-            <form role="form">
+            <form role="form" method="POST">
                 <fieldset>
                     <div class="col-xs-10">
                         <h2 class="login-heading">SignIn to Take Your Leave</h2>
