8d2a778f3b5f2747e08b1fb396a03dc5e3a04433
Sajitha Liyanage <sajithaliyanage@gmail.com>
2016-06-06 14:19:26 +0530

    navbar-updated

--- admin/navbar.php
+++ admin/navbar.php
@@ -26,7 +26,7 @@
                 <li class="list-side-menu"><a href="#">Edit Company Calendar</a></li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle font new" data-toggle="dropdown" role="button"
-                       aria-haspopup="true" aria-expanded="false"><img src="../images/emp.png" class="img-circle image-user"  />Sajitha Liyanage <span class="caret"></span></a>
+                       aria-haspopup="true" aria-expanded="false"><img src="images/emp.png" class="img-circle image-user"  />Sajitha Liyanage <span class="caret"></span></a>
                     <ul class="dropdown-menu dropdown-menu-admin">
                         <li><a href="#">Switch Profile</a></li>
                         <li><a href="#">Logout</a></li>
