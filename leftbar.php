c5b379ecc1e3332d45fd963efce3e803fd4a8568
Sajitha Liyanage <sajithaliyanage@gmail.com>
2016-06-09 19:17:30 +0530

    user navbar updated

--- leftbar.php
+++ leftbar.php
@@ -1,9 +1,24 @@
-<div class="list-group" style="margin-top:10px;">
-            <a href="index.php" class="list-group-item left-menu left-menu-active"><i class="fa fa-building icon-bar" aria-hidden="true"></i>Overall Company</a>
-            <a href="department.php" class="list-group-item left-menu"><i class="fa fa-user-plus icon-bar" aria-hidden="true"></i>Edit Employees</a>
-            <a href="setleaves.php" class="list-group-item left-menu"><i class="fa fa-plus icon-bar" aria-hidden="true"></i>Set Leaves</a>
-            <a href="jobcategory.php" class="list-group-item left-menu"><i class="fa fa-briefcase icon-bar" aria-hidden="true"></i>Edit Job Categories</a>
-            <a href="leavetypes.php" class="list-group-item left-menu"><i class="fa fa-envelope-o icon-bar" aria-hidden="true"></i>Edit Leave types</a>
-            <a href="roster.php" class="list-group-item left-menu"><i class="fa fa-shirtsinbulk icon-bar" aria-hidden="true"></i>Edit Roster System</a>
-            <a href="#" class="list-group-item left-menu"><i class="fa fa-calendar icon-bar" aria-hidden="true"></i>Edit Company Calendar</a>
+
+<div class="list-group" style="margin-top:-10px;">
+    <center>
+        <div class="admin-panel-heading">
+            <p  class=" font new"  role="button"
+                aria-haspopup="true" aria-expanded="false" style="margin-top:25px;"><img src="images/new-logo.png" class="img-circle image-user"  />Take Your Leave</p>
+        </div>
+    </center>
+
+    <br/>
+    <center>
+        <div style="padding:10px 0;">
+            <p  class=" font new" style="margin-top:-10px;"><img src="images/emp.png" class="img-circle image-user-nav"  />Welcome Sajitha</p>
+        </div>
+    </center>
+    <a href="" class="list-group-item left-menu left-menu-active"><i class="fa fa-building icon-bar" aria-hidden="true"></i>Apply Leave<span class="selected"></span></a>
+    <a href="" class="list-group-item left-menu left-menu"><i class="fa fa-building icon-bar" aria-hidden="true"></i>Edit Department</a>
+    <a href="" class="list-group-item left-menu left-menu"><i class="fa fa-user-plus icon-bar" aria-hidden="true"></i>Edit Employees</a>
+    <a href="" class="list-group-item left-menu left-menu"><i class="fa fa-plus icon-bar" aria-hidden="true"></i>Set Leaves</a>
+    <a href="" class="list-group-item left-menu left-menu"><i class="fa fa-briefcase icon-bar" aria-hidden="true"></i>Edit Job Categories</a>
+    <a href="" class="list-group-item left-menu left-menu"><i class="fa fa-envelope-o icon-bar" aria-hidden="true"></i>Edit Leave types</a>
+    <a href="" class="list-group-item left-menu left-menu"><i class="fa fa-shirtsinbulk icon-bar" aria-hidden="true"></i>Edit Roster System</a>
+    <a href="#" class="list-group-item left-menu left-menu"><i class="fa fa-calendar icon-bar" aria-hidden="true"></i>Edit Company Calendar</a>
 </div>
