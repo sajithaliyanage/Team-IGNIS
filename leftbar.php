591578ce7f2fb34800b31bc10c646e0ba07b7ea2
Gothami94 <gothamikarunarathna@gmail.com>
2016-06-15 19:23:42 +0530

    correct MY Calendar in leftbar-> My Calendar

--- leftbar.php
+++ leftbar.php
@@ -18,11 +18,9 @@
     <a href="attendance.php" class="list-group-item left-menu left-menu-active"><i class="fa fa-check-circle icon-bar" aria-hidden="true"></i>My Attendance<span class="<?php if($var=="attendance"){echo"selected";}?>"></span></a>
     <a href="" class="list-group-item left-menu left-menu"><i class="fa fa-plus icon-bar" aria-hidden="true"></i>Medical Upload Center<span class="<?php if($var=="medical"){echo"selected";}?>"></span></a>
     <a href="" class="list-group-item left-menu left-menu"><i class="fa fa-briefcase icon-bar" aria-hidden="true"></i>Generate Reports<span class="<?php if($var=="reports"){echo"selected";}?>"></span></a>
-<<<<<<< HEAD
+
     <a href="" class="list-group-item left-menu left-menu"><i class="fa fa-calendar icon-bar" aria-hidden="true"></i>My Calendar<span class="<?php if($var=="calendar"){echo"selected";}?>"></span></a>
-=======
-    <a href="" class="list-group-item left-menu left-menu"><i class="fa fa-calendar icon-bar" aria-hidden="true"></i>MY Calendar<span class="<?php if($var=="calendar"){echo"selected";}?>"></span></a>
->>>>>>> 345d83df60d26a556d90e18a7c5fca5aad2983e7
+
     <a href="roster.php" class="list-group-item left-menu left-menu"><i class="fa fa-shirtsinbulk icon-bar" aria-hidden="true"></i>Roster System<span class="<?php if($var=="roster"){echo"selected";}?>"></span></a>
     <a href="#" class="list-group-item left-menu left-menu"><i class="fa fa-user icon-bar" aria-hidden="true"></i>My Profile<span class="<?php if($var=="profile"){echo"selected";}?>"></span></a>
 
