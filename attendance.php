bdd9588622ea8ab34087ac74c8fbe08ea4dd56bc
NilakshaDeemantha <nilakshahp@gmail.com>
2016-06-15 10:31:49 +0530

    attendance

--- attendance.php
+++ attendance.php
@@ -30,99 +30,57 @@ $var = "attendance";
             <?php include ("leftbar.php")?>
         </div>
 
-        <div class="col-sm-10 col-xs-12 col-sm-push-2">
-
-            <div class="col-sm-6 col-xs-12 padding-box">
+        <div class="col-sm-10 col-xs-12 col-sm-push-2 padding-box">
+            <div class="row padding-row">
                 <div class="row">
-                    <div class="col-xs-12 nortification-box-top">
-                        <h5 class="nortification-box-heading"><i class="fa fa-tag icon-margin-right" aria-hidden="true"></i>
-                            Set Leaves for Job Categories</h5>
-                        <hr>
-                        <form role="form" data-toggle="validator" action="" method="post">
-                            <div class="department-add">
-                                <div class="col-xs-12">
-                                    <!-- Text input-->
-                                    <div class="form-group">
-                                        <label class="col-xs-5 control-label form-lable">Job Category :</label>
-                                        <div class="col-xs-7">
-                                            <input id="service_name" name="job_category" type="text" placeholder=""
-                                                   class="form-control input-md" required>
-                                        </div>
-                                    </div>
-                                    <br>
-                                    <br>
-
-                                    <div class="form-group">
-                                        <label class="col-xs-5 control-label form-lable">Job Level :</label>
-                                        <div class="col-xs-7">
-                                            <select  name="emp_role" class="form-control">
-                                                <option>-Select-</option>
-                                                <option value="YES">Permanent</option>
-                                                <option value="NO">Probation</option>
-                                                <option value="NO">Trainee</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <br>
-                                    <br>
-
-                                    <div class="form-group">
-                                        <label class="col-xs-5 control-label form-lable">Annual Leaves :</label>
-                                        <div class="col-xs-7">
-                                            <input id="service_name" name="job_category" type="text" placeholder=""
-                                                   class="form-control input-md" required>
-                                        </div>
-                                    </div>
-                                    <br>
-                                    <br>
-
-                                    <div class="form-group">
-                                        <label class="col-xs-5 control-label form-lable">Casual Leaves :</label>
-                                        <div class="col-xs-7">
-                                            <input id="service_name" name="job_category" type="text" placeholder=""
-                                                   class="form-control input-md" required>
-                                        </div>
-                                    </div>
-                                    <br>
-                                    <br>
-
-                                    <div class="form-group">
-                                        <label class="col-xs-5 control-label form-lable">Medical Leaves :</label>
-                                        <div class="col-xs-7">
-                                            <input id="service_name" name="job_category" type="text" placeholder=""
-                                                   class="form-control input-md" required>
-                                        </div>
-                                    </div>
-                                    <br>
-                                    <br>
-
-                                    <div class="form-group">
-                                        <label class="col-xs-5 control-label form-lable">Half Days</label>
-                                        <div class="col-xs-7">
-                                            <input id="service_name" name="job_category" type="text" placeholder=""
-                                                   class="form-control input-md" required>
-                                        </div>
-                                    </div>
-                                    <br>
-                                    <br>
-
-                                    <div class="form-group">
-                                        <label class="col-xs-5 control-label form-lable">Monthly Short Leaves :</label>
-                                        <div class="col-xs-7">
-                                            <input id="service_name" name="job_category" type="text" placeholder=""
-                                                   class="form-control input-md" required>
-                                        </div>
-                                    </div>
-                                    <br>
-                                    <br>
-
-                                    <button class="btn btn-info btn-lg pull-right submit-button" type="submit">Submit</button>
-                                </div>
-                            </div>
-                        </form>
+                    <div class="col-lg-12">
+                        <ol class="breadcrumb breadcrumb-style">
+                            <li>
+                                <i class="fa fa-building"></i>  <a href="attendance.php">Apply Leave</a>
+                            </li>
+                            <li class="active">
+                                <i class="fa fa-user-plus"></i> My Attendance
+                            </li>
+                        </ol>
                     </div>
                 </div>
+            <div class="row padding-row">
+                <div class="col-sm-6 col-xs-12 padding-box">
+                    <form class="container ">
+                        <div class="col-md-6 col-xs-12">
+                            <ul>
+                                <li><h4>&nbsp;&nbsp;&nbsp;Select Date Range</h4></li>
+
+                                <li>
+
+                                    <label class="col-md-5 col-col-xs-12 " for="start_date fm fm">Start Date   :</label>
+                                    <input class="col-md-7 col-xs-12" type="date" name="start_date" id="start_date"/>
+                                    <br>
+                                    <br>
+                                </li>
+                                <li>
+                                    <label  class="col-md-5 col-xs-12" for="end_date">End Date   :</label>
+                                    <input  class="col-md-7 col-xs-12" type="date" name="end_date" id="end_date"/>
+                                    <br>
+                                    <br>
+                                </li>
+
+                                <li><button class="col-md-7 col-xs-12 btn btn-info btn-lg pull-right submit-button" type="submit">Filter</button></li>
+                            </ul>
+
+                        </div>
+                        <div class=" col-md-6 col-xs-12" id="leave_type">
+                            <div class="container-fluid pull-right">
+                                <br>
+
+                                <h4><i class="fa fa-square " style="font-size: 40px;color: #00a65a" aria-hidden="true"></i>&nbsp;&nbsp;Approved Leave</h4>
+                                <br>
+                                <h4><i class="fa fa-square " style="font-size: 40px;color: #ac2925" aria-hidden="true"></i>&nbsp;&nbsp;Unplanned Absences</h4>
+                            </div>
+                        </div>
+                    </form>
             </div>
+
         </div>
          </div>
       </div>
