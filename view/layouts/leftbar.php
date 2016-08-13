4da8e4411652e5f9ac0c748f3315182c77163076
Dileepjayasundara <dileepjayasundara@gmail.com>
2016-08-13 10:48:01 +0530

    Leave status
    interface

--- view/layouts/leftbar.php
+++ view/layouts/leftbar.php
@@ -13,10 +13,15 @@
             <p  class=" font new" style="margin-top:-10px;"><img src="../../public/images/emp.png" class="img-circle image-user-nav"  />Welcome Sajitha</p>
         </div>
     </center>
+
     <a href="../site/apply.php" class="list-group-item left-menu left-menu<?php if($var=="apply"){echo"-active";}?>"><i class="fa fa-edit icon-bar" aria-hidden="true"></i>Apply Leave<span class="<?php if($var=="apply"){echo"selected";}?>"></span></a>
-    <a href="" class="list-group-item left-menu left-menu<?php if($var=="status"){echo"-active";}?>"><i class="fa fa-building icon-bar" aria-hidden="true"></i>Leave Status<span class="<?php if($var=="status"){echo"selected";}?>"></span></a>
+
+    <a href="../site/leave_status.php" class="list-group-item left-menu left-menu<?php if($var=="status"){echo"-active";}?>"><i class="fa fa-building icon-bar" aria-hidden="true"></i>Leave Status<span class="<?php if($var=="status"){echo"selected";}?>"></span></a>
+
     <a href="../site/attendance.php" class="list-group-item left-menu left-menu<?php if($var=="attendance"){echo"-active";}?>"><i class="fa fa-check-circle icon-bar" aria-hidden="true"></i>My Attendance<span class="<?php if($var=="attendance"){echo"selected";}?>"></span></a>
+
     <a href="../site/medicalUpload.php" class="list-group-item left-menu left-menu<?php if($var=="medical"){echo"-active";}?>"><i class="fa fa-plus icon-bar" aria-hidden="true"></i>Medical Upload Center<span class="<?php if($var=="medical"){echo"selected";}?>"></span></a>
+
     <a href="../site/generateReport.php" class="list-group-item left-menu left-menu<?php if($var=="reports"){echo"-active";}?>"><i class="fa fa-briefcase icon-bar" aria-hidden="true"></i>Generate Reports<span class="<?php if($var=="reports"){echo"selected";}?>"></span></a>
 
     <a href="" class="list-group-item left-menu left-menu<?php if($var=="calendar"){echo"-active";}?>"><i class="fa fa-calendar icon-bar" aria-hidden="true"></i>My Calendar<span class="<?php if($var=="calendar"){echo"selected";}?>"></span></a>
---
+++ view/site/leave_status.php
@@ -0,0 +1,152 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Take Your Leave | Online</title>
+
+    <link href="../../public/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../public/css/interface-leftmenu.css" rel="stylesheet">
+    <link href="../../public/css/leave-interface.css" rel="stylesheet">
+    <link href="../../public/css/navbar-style.css" rel="stylesheet">
+    <link href="../../public/css/font-awesome.min.css" rel="stylesheet">
+
+</head>
+
+<body style=" background-color: #eceff4 !important;">
+
+<?php include("../layouts/navbar.php") ?>
+
+<div class="container-fluid ">
+    <div class="row ">
+
+        <div class="col-sm-2 col-xs-12 left-menu-div side-bar-display">
+            <?php include("../layouts/leftbar.php") ?>
+        </div>
+
+        <div class="col-sm-10 col-xs-12 admin-background col-sm-push-2" style="position: relative;">
+
+            <div class="row padding-row">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <ol class="breadcrumb breadcrumb-style">
+                            <li>
+                                <i class="fa fa-dashboard"></i>  <a href="../../index.php">Take Your Leave</a>
+                            </li>
+                            <li class="active">
+                                <i class="fa fa-building" ></i> Leave Status
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row padding-row">
+                <div class="col-sm-7 col-xs-12 padding-box">
+                    <div class="row">
+                        <div class="col-xs-12 nortification-box-top">
+                            <h5 class="nortification-box-heading"><i class="fa fa-calendar-check-o icon-margin-right" aria-hidden="true"></i>
+                                leaves pending for approval or rejection</h5>
+
+                            <hr>
+
+                            
+                            <div class="right">
+                                <ul class="pager ">
+                                    <li class="previous btn-sm"><a href="#"">Previous</a></li>
+                                    <li class="next btn-sm"><a href="#">Next</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="list-group">
+                                <li class="list-group-item" style="background-color:#ccd7f8 "><h5 >Leave applied by :Dileep Jayasundara</h5></li>
+                            </div>
+
+                            <div class="list-group">
+
+                                <li class="list-group-item">Duration : 2016/08/12 to 2016/08/13</li>
+                                <li class="list-group-item">Reason :personal issue</li>
+                                <li class="list-group-item">special note :<input type="textarea" name=""> <button class="btn btn-success btn-sm">Approve</button> <button class="btn btn-danger btn-sm">Reject</button><button type="button" class="btn btn-link btn-xs">cancel Leave</button></li>
+
+
+
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class="row margin-top">
+                        <div class="col-xs-12 nortification-box-top">
+                            <h5 class="nortification-box-heading"><i class="fa fa-list icon-margin-right"
+                                                                     aria-hidden="true"></i>
+                                Past Leave Notifications</h5>
+                            <hr>
+                            <div class="list-group">
+                                <a href="#" class="list-group-item">20/05/2016<span style="float:right;"> Approved <i
+                                            class="fa fa-check" aria-hidden="true"></i></span></a>
+                                <a href="#" class="list-group-item">18/05/2016<span style="float:right;"> Approved <i
+                                            class="fa fa-check" aria-hidden="true"></i></span></a>
+                                <a href="#" class="list-group-item">16/03/2016<span style="float:right;"> Approved <i
+                                            class="fa fa-check" aria-hidden="true"></i></span></a>
+                                <a href="#" class="list-group-item">02/01/2016<span style="float:right;"> Approved <i
+                                            class="fa fa-check" aria-hidden="true"></i></span></a>
+                                <a href="#" class="list-group-item">07/11/2015<span style="float:right;"> Rejected <i
+                                            class="fa fa-close" aria-hidden="true"></i></span></a>
+                                <a href="#" class="list-group-item">27/05/2015<span style="float:right;"> Approved <i
+                                            class="fa fa-check" aria-hidden="true"></i></span></a>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-sm-5 col-xs-12 padding-box">
+                    <div class="row">
+                        <div class="col-xs-12 nortification-box-top">
+                            <h5 class="nortification-box-heading"><i class="fa fa-angle-double-right " aria-hidden="true"></i>
+                                 Leave requests on same time</h5>
+                            <hr>
+                            <div class="list-group-item-heading">
+                                <a href="#" class="list-group-item"><i class="fa fa-user" aria-hidden="true"></i> Name  <span style="float:right;"> Date <i class="fa fa-clock-o fa-lg" aria-hidden="true"></i></span></a>
+                            </div>
+
+                            <div class="list-group">
+                                <a href="#" class="list-group-item">Sajitha Liayanage<span style="float:right;"> 2016/08/01 </span></a>
+                                <a href="#" class="list-group-item">Priyanwada Kulasuriya<span style="float:right;"> 2016/08/01 </span></a>
+                                <a href="#" class="list-group-item">Gothami Gunarathne<span style="float:right;"> 2016/08/01 </span></a>
+                                <a href="#" class="list-group-item">Madusha Ushan<span style="float:right;"> 2016/08/01 </span></a>
+
+
+                            </div>
+                            
+                        </div>
+                    </div>
+
+                    <div class="row margin-top">
+                        <div class="col-xs-12 nortification-box-top">
+                            <h5 class="nortification-box-heading"><i class="fa fa-calendar icon-margin-right" aria-hidden="true"></i>
+                                Profile Calendar</h5>
+                            <div style="height:300px;background-color:#4cae4c; margin-bottom:20px;">
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+<script src="../../public/js/jquery.js"></script>
+<script src="../../public/js/bootstrap.js"></script>
+</body>
+</html>
