cbcbf763bc17b24b52a68ae5c92b7642d6fb9ca8
Madushaushan <djmadushaushan@gmail.com>
2016-08-25 13:48:18 +0530

    edited

--- view/site/roster.php
+++ view/site/roster.php
@@ -48,6 +48,40 @@ include('../../controller/siteController.php');
 				</div>
 				
 			<div class="col-sm-6 col-xs-12">
+
+
+                <div class="w3-content" style="max-width:800px;position:relative">
+
+                    <h1 class="mySlides" align="middle">TEXT 1</h1>
+                    <h1 class="mySlides" align="middle">TEXT 2</h1>
+                    <h1 class="mySlides" align="middle">TEXT 3</h1>
+
+
+
+                    <a class="w3-btn-floating" style="position:absolute;top:45%;left:0" onclick="plusDivs(-1)">❮</a>
+                    <a class="w3-btn-floating" style="position:absolute;top:45%;right:0" onclick="plusDivs(1)">❯</a>
+
+                </div>
+
+                <script>
+                    var slideIndex = 1;
+                    showDivs(slideIndex);
+
+                    function plusDivs(n) {
+                        showDivs(slideIndex += n);
+                    }
+
+                    function showDivs(n) {
+                        var i;
+                        var x = document.getElementsByClassName("mySlides");
+                        if (n > x.length) {slideIndex = 1}
+                        if (n < 1) {slideIndex = x.length}
+                        for (i = 0; i < x.length; i++) {
+                            x[i].style.display = "none";
+                        }
+                        x[slideIndex-1].style.display = "block";
+                    }
+                </script>
 				</div>
 				<div class="col-sm-6 col-xs-12">		
 					<div class="col-xs-12 nortification-box-top">
