aae326b9be4e9774fb1f689c802bd205398fe3dc
Sajitha Liyanage <sajithaliyanage@gmail.com>
2016-12-03 12:25:41 +0530

    updated

---
+++ view/layouts/onlineStatus.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<div class="btn-group dropup online-status">
+    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="padding:5px 40px;">
+        Online Status (23)
+        <span class="caret"></span>
+        <span class="sr-only">Toggle Dropdown</span>
+    </button>
+    <ul id="theList" class="dropdown-menu" style="max-height:70vh; overflow-x: hidden;height: auto; width:260px;">
+        <input id="iconified" class="form-control empty" type="text" placeholder="&#xF002; Search" onkeyup="filter(this,'theList')" style="width:260px; background-color:#f4f4f4; height:30px;" />
+       <li><a href="#"> <i class="fa fa-circle" aria-hidden="true" style="color: #398439; margin-top:12px; font-size:10px; margin-right:10px;"></i><img src="images/default.png" style="width:30px; height:30px; margin-right:10px;" />Sajitha Liyanage</a></li>
+    </ul>
+</div>
