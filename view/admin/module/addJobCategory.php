d07e58fe18fe9cc1c0b3307c98442ca8a60019bd
Madushaushan <Djmadushaushan@gmail.com>
2017-01-13 15:55:23 +0530

    update

--- view/admin/module/addJobCategory.php
+++ view/admin/module/addJobCategory.php
@@ -5,16 +5,38 @@ $pdo = connect();
 //post request data
 $catName = $_POST['job_category'];
 
-try{
+$smt = "SELECT job_cat_name FROM job_category where job_cat_name =:log AND (currentStatus =:log1 OR currentStatus=:log2) ";
+$query = $pdo->prepare($smt);
+$query->execute(array('log'=>$catName,'log1'=>"approved",'log2'=>'waiting'));
+$result = $query->fetch();
+$rownum = $query->rowCount();
+
+
+$flag=1;
+if (empty($catName)) {
+    $flag = 0;
+}else if($rownum>0){
+    $flag = 0;
+
+}
+
+if($flag==1){
+
+
+    try {
 //    create new job category for company
-    $sql = "INSERT INTO job_category (job_cat_name,currentStatus) VALUES (:catName,:ststus)";
-    $query = $pdo->prepare($sql);
-    $query->execute(array('catName'=>$catName,'ststus'=>"waiting"));
+        $sql = "INSERT INTO job_category (job_cat_name,currentStatus) VALUES (:catName,:ststus)";
+        $query = $pdo->prepare($sql);
+        $query->execute(array('catName' => $catName, 'ststus' => "waiting"));
 
-    header("Location:../jobcategory.php?job=done");
+        header("Location:../jobcategory.php?job=done");
 
-}catch(PDOException $e){
-    header("Location:../../view/layouts/error.php");
+    } catch (PDOException $e) {
+        header("Location:../../view/layouts/error.php");
+    }
+}
+else{
+    header("Location:../jobcategory.php?jobCaterror");
 }
 
 
--- view/admin/module/addJobLevels.php
+++ view/admin/module/addJobLevels.php
@@ -5,16 +5,35 @@ $pdo = connect();
 //post request data
 $levelname = $_POST['level_name'];
 
-try{
+$smt = "SELECT level_name FROM job_level where level_name =:log";
+$query = $pdo->prepare($smt);
+$query->execute(array('log'=>$levelname));
+$result = $query->fetch();
+$rownum = $query->rowCount();
+
+$flag=1;
+if (empty($levelname)) {
+    $flag = 0;
+}else if($rownum>0){
+    $flag = 0;
+
+}
+
+if($flag==1) {
+    try {
 //    create new job level for company
-    $sql = "INSERT INTO job_level (level_name) VALUES (:catName)";
-    $query = $pdo->prepare($sql);
-    $query->execute(array('catName'=>$levelname));
+        $sql = "INSERT INTO job_level (level_name) VALUES (:catName)";
+        $query = $pdo->prepare($sql);
+        $query->execute(array('catName' => $levelname));
 
-    header("Location:../jobcategory.php?job1=done");
+        header("Location:../jobcategory.php?job1=done");
 
-}catch(PDOException $e){
-    header("Location:../../view/layouts/error.php");
+    } catch (PDOException $e) {
+        header("Location:../../view/layouts/error.php");
+    }
+}
+else{
+    header("Location:../jobcategory.php?jobLevelerror");
 }
 
 
