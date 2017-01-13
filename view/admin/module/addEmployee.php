37cb351e95e5c6434276bba35151fcc4207e1d88
Madushaushan <Djmadushaushan@gmail.com>
2017-01-13 12:42:59 +0530

    push

--- view/admin/module/addEmployee.php
+++ view/admin/module/addEmployee.php
@@ -84,96 +84,104 @@ if (empty($deptId)) {
     $flag = 0;
 } else if (!filter_var($empEmail, FILTER_VALIDATE_EMAIL)) {
     $flag = 0;
-} else if (empty($rownum != 0)) {
+} else if (intval($rownum) != 0) {
     $flag = 0;
 }
 
-if ($flag == 1) {
+try{
 
-//    incrment number of employee by 1
-    $sql = "UPDATE department SET no_of_emp =no_of_emp+1 WHERE dept_id=:depID";
-    $query = $pdo->prepare($sql);
-    $query->execute(array('depID' => $deptId));
 
-//    add employee to employee table
-    $sql = "INSERT INTO employee (comp_id,name,nic,gender,email,password,tel_no,image,job_cat_id,level_id,dept_id) VALUES
-            (:compId,:empName,:empNIC,:empGender,:empEmail,:empPassword,:empTel,:empImage,:empJob,:empLevel,:empDept)";
-    $query = $pdo->prepare($sql);
-    $query->execute(array('compId' => $empId, 'empName' => $empName, 'empNIC' => $empNIC, 'empGender' => $empGender, 'empEmail' => $empEmail,
-        'empPassword' => $empPassword, 'empTel' => $empTelephone, 'empImage' => "null", 'empJob' => $empCategory, 'empLevel' => $empLevel, 'empDept' => $deptId));
+    if ($flag == 1) {
 
-//    add employee to specific table
-    if ($empRole == 'director') {
-        $sql = "INSERT INTO director (comp_id) VALUES (:comp_id)";
+    //    incrment number of employee by 1
+        $sql = "UPDATE department SET no_of_emp =no_of_emp+1 WHERE dept_id=:depID";
         $query = $pdo->prepare($sql);
-        $query->execute(array('comp_id' => $empId));
+        $query->execute(array('depID' => $deptId));
 
-    } else if ($empRole == 'admin') {
-        $sql = "INSERT INTO admin (comp_id,dept_id) VALUES (:comp_id,:dept_id)";
+    //    add employee to employee table
+        $sql = "INSERT INTO employee (comp_id,name,nic,gender,email,password,tel_no,image,job_cat_id,level_id,dept_id) VALUES
+                (:compId,:empName,:empNIC,:empGender,:empEmail,:empPassword,:empTel,:empImage,:empJob,:empLevel,:empDept)";
         $query = $pdo->prepare($sql);
-        $query->execute(array('comp_id' => $empId, 'dept_id' => $deptId));
+        $query->execute(array('compId' => $empId, 'empName' => $empName, 'empNIC' => $empNIC, 'empGender' => $empGender, 'empEmail' => $empEmail,
+            'empPassword' => $empPassword, 'empTel' => $empTelephone, 'empImage' => "null", 'empJob' => $empCategory, 'empLevel' => $empLevel, 'empDept' => $deptId));
 
-    } else if ($empRole == 'manager') {
-        $sql = "INSERT INTO manager (comp_id,dept_id) VALUES (:comp_id,:dept_id)";
-        $query = $pdo->prepare($sql);
-        $query->execute(array('comp_id' => $empId, 'dept_id' => $deptId));
+    //    add employee to specific table
+        if ($empRole == 'director') {
+            $sql = "INSERT INTO director (comp_id) VALUES (:comp_id)";
+            $query = $pdo->prepare($sql);
+            $query->execute(array('comp_id' => $empId));
 
-    } else if ($empRole == 'executive') {
-        $sql = "INSERT INTO executive (comp_id,dept_id) VALUES (:comp_id,:dept_id)";
-        $query = $pdo->prepare($sql);
-        $query->execute(array('comp_id' => $empId, 'dept_id' => $deptId));
+        } else if ($empRole == 'admin') {
+            $sql = "INSERT INTO admin (comp_id,dept_id) VALUES (:comp_id,:dept_id)";
+            $query = $pdo->prepare($sql);
+            $query->execute(array('comp_id' => $empId, 'dept_id' => $deptId));
 
-    } else if ($empRole == 'employee') {
-        $sql = "INSERT INTO general_employee (comp_id,dept_id) VALUES (:comp_id,:dept_id)";
+        } else if ($empRole == 'manager') {
+            $sql = "INSERT INTO manager (comp_id,dept_id) VALUES (:comp_id,:dept_id)";
+            $query = $pdo->prepare($sql);
+            $query->execute(array('comp_id' => $empId, 'dept_id' => $deptId));
+
+        } else if ($empRole == 'executive') {
+            $sql = "INSERT INTO executive (comp_id,dept_id) VALUES (:comp_id,:dept_id)";
+            $query = $pdo->prepare($sql);
+            $query->execute(array('comp_id' => $empId, 'dept_id' => $deptId));
+
+        } else if ($empRole == 'employee') {
+            $sql = "INSERT INTO general_employee (comp_id,dept_id) VALUES (:comp_id,:dept_id)";
+            $query = $pdo->prepare($sql);
+            $query->execute(array('comp_id' => $empId, 'dept_id' => $deptId));
+
+        }
+
+    //    get set id for create leave count
+        $sql = "SELECT set_id FROM leave_set_job WHERE job_cat_id=:jobCat and level_id=:jobLevel";
         $query = $pdo->prepare($sql);
-        $query->execute(array('comp_id' => $empId, 'dept_id' => $deptId));
+        $query->execute(array('jobCat' => $empCategory, 'jobLevel' => $empLevel));
+        $result = $query->fetchAll();
+        $setID = 0;
+        foreach ($result as $rs) {
+            $setID = $rs['set_id'];
+        }
+
+        $sql = "SELECT * FROM leave_count_details WHERE set_id=:setID";
+        $query = $pdo->prepare($sql);
+        $query->execute(array('setID' => $setID));
+        $result = $query->fetchAll();
+
+        $empLeaveCount = array();
+        foreach ($result as $rs) {
+            array_push($empLeaveCount, $rs['leave_count']);
+        }
+
+    //    set leave types and count for new employee
+        $sql0 = "SELECT leave_id FROM leave_type";
+        $query0 = $pdo->prepare($sql0);
+        $query0->execute();
+        $result0 = $query0->fetchAll();
+
+        $sql2 = "";
+        $expo = array();
+        $count = 0;
+        foreach ($empLeaveCount as $number) {
+
+            $sql2 .= "INSERT INTO employee_leave_count(comp_id,leave_id,leave_count)VALUES(?,?,?);";
+            array_push($expo, $empId);
+            array_push($expo, $result0[$count][0]);
+            array_push($expo, $number);
+            $count++;
+        }
+        $query2 = $pdo->prepare($sql2);
+        $query2->execute($expo);
+
+        header("Location:../employee.php?job=done");
+
+    } else {
+        header("Location:../employee.php?error");
 
     }
+}catch (PDOException $e){
+    header("Location:../../view/layouts/error.php");
 
-//    get set id for create leave count
-    $sql = "SELECT set_id FROM leave_set_job WHERE job_cat_id=:jobCat and level_id=:jobLevel";
-    $query = $pdo->prepare($sql);
-    $query->execute(array('jobCat' => $empCategory, 'jobLevel' => $empLevel));
-    $result = $query->fetchAll();
-    $setID = 0;
-    foreach ($result as $rs) {
-        $setID = $rs['set_id'];
-    }
-
-    $sql = "SELECT * FROM leave_count_details WHERE set_id=:setID";
-    $query = $pdo->prepare($sql);
-    $query->execute(array('setID' => $setID));
-    $result = $query->fetchAll();
-
-    $empLeaveCount = array();
-    foreach ($result as $rs) {
-        array_push($empLeaveCount, $rs['leave_count']);
-    }
-
-//    set leave types and count for new employee
-    $sql0 = "SELECT leave_id FROM leave_type";
-    $query0 = $pdo->prepare($sql0);
-    $query0->execute();
-    $result0 = $query0->fetchAll();
-
-    $sql2 = "";
-    $expo = array();
-    $count = 0;
-    foreach ($empLeaveCount as $number) {
-
-        $sql2 .= "INSERT INTO employee_leave_count(comp_id,leave_id,leave_count)VALUES(?,?,?);";
-        array_push($expo, $empId);
-        array_push($expo, $result0[$count][0]);
-        array_push($expo, $number);
-        $count++;
-    }
-    $query2 = $pdo->prepare($sql2);
-    $query2->execute($expo);
-
-    header("Location:../employee.php?job=done");
-
-} else {
-    header("Location:../employee.php?error");
 
 }
 
