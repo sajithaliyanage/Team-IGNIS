2a3c4d69dea52035d4ff424a163a72ff1db2ed0d
Gothami94 <gothamikarunarathna@gmail.com>
2017-01-13 11:07:44 +0530

    updated

--- view/site/graphGenerator.php
+++ view/site/graphGenerator.php
@@ -14,7 +14,56 @@ $eDate=str_replace('/', '-', $eDate);
 $endDate=date("Y-m-d", strtotime($eDate) );
 $d1=strtotime($startDate);
 $d2=strtotime($endDate);
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Take Your Leave | Online</title>
+
+    <link href="../admin/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../admin/css/interface-leftmenu.css" rel="stylesheet">
+    <link href="../admin/css/adminpanel-interface.css" rel="stylesheet">
+    <link href="../admin/css/navbar-style.css" rel="stylesheet">
+    <link href="../admin/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../public/css/datepicker.css">
+    <link rel="stylesheet" href="../../public/css/attendance.css">
+
+
+</head>
+
+<body style=" background-color: #eceff4 !important;">
+<!---top nav bar--->
+<?php include("../layouts/navbar.php") ?>
+
+<div class="container-fluid ">
+    <div class="row ">
+        <!--left nav bar--->
+        <div class="col-sm-2 col-xs-12 left-menu-div side-bar-display">
+            <?php include("../layouts/leftbar.php") ?>
+        </div>
+
+        <div class="col-sm-10 col-xs-12 admin-background col-sm-push-2" style="position: relative;">
+          <div class="row padding-row">
+              <div class="row">
+                  <div class="col-lg-12">
+                      <ol class="breadcrumb breadcrumb-style">
+                          <li>
+                              <i class="fa fa-dashboard"></i> <a href="director.php">Take Your Leave</a>
+                          </li>
+                          <li class="active">
+                              <i class="fa fa-bar-chart"></i> Company Attendance Analyse
+                          </li>
+                      </ol>
+                  </div>
+              </div>
+          </div>
+<?php
     //<!-- get the attendance from excel sheet -->
 
       try{
@@ -31,6 +80,7 @@ $d2=strtotime($endDate);
                 $i+=1;
             }
         }
+        $num=count($id);
         $num1=count($id);
 
         //get the dates from excel sheet
@@ -42,10 +92,126 @@ $d2=strtotime($endDate);
                 $i+=1;
             }
         }
-
+        //print_r($dates);
         }catch(Exception $e){
-        echo $e;
+          echo $e;
         }
+?>
+        <!---start top boxes--->
+        <div class="row padding-row">
+            <div class="col-sm-5 col-xs-12 padding-box">
+                <div class="row">
+                    <div class="col-xs-8 main-box-1-1">
+                        <div class="row">
+                          <div class="col-xs-4">
+                                <i class="fa fa-group fa-5x box-icon" aria-hidden="true"></i>
+                          </div>
+                          <div class="col-xs-8">
+                                <center><h3 class="box-head">Today<br> Presents</h3></center>
+                          </div>
+                        </div>
+                    </div>
+                <div class="col-sm-4 col-xs-12" style="background-color:#FFFFFF; height:110px;">
+                        <div class="row">
+                          <?php
+                              $sql="SELECT * FROM employee";
+                              $query = $pdo->prepare($sql);
+                              $query->execute();
+                              $numrow = $query->rowCount();
+                              $numrows = intval($numrow);
+                              $precetage=($num/($numrows))*100;
+                          ?>
+                          <center><h2 class="box-count"><?php if($num<10){echo "0".$num;}else{echo $num;}?><br><?php echo round($precetage);?>%</h2></center>
+                        </div>
+                </div>
+            </div>
+          </div>
+            <div class="col-sm-2 col-xs-12 padding-box">
+                <div class="row">
+                </div>
+            </div>
+
+            <div class="col-sm-5 col-xs-12 padding-box">
+                <div class="row">
+                    <div class="col-sm-8 col-xs-12 main-box-4-1">
+                        <div class="row">
+                          <div class="col-xs-4">
+                                <i class="fa fa-eye fa-5x box-icon" aria-hidden="true"></i>
+                          </div>
+                          <div class="col-xs-8">
+                                <center><h3 class="box-head">Today<br> Absents</h3></center>
+                          </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 col-xs-12" style="background-color:#ffffff; height:110px;">
+                        <div class="raw">
+                          <?php
+                          $sql="SELECT * FROM employee";
+                          $query = $pdo->prepare($sql);
+                          $query->execute();
+                          $numrow = $query->rowCount();
+                          $numrows = intval($numrow);
+                          $absent=intval($numrows-$num);
+                          $precetage=($absent/($numrows))*100;
+                          ?>
+                          <center><h2 class="box-count"><?php if($absent<10){echo "0".$absent;}else{echo $absent;}?><br><?php echo round($precetage);?>%</h2></center>
+                        </div>
+            </div>
+        </div>
+      </div>
+    </div>
+    <br><br>
+      <!---end top boxes--->
+    <div class="row padding-row"  >
+        <div class="col-sm-12 col-xs-12 padding-row">
+            <div class="row">
+                <div class="col-xs-12 nortification-box-top">
+                    <h5 class="nortification-box-heading"><i class="fa fa-bar-chart icon-margin-right" aria-hidden="true"></i>
+                          Overall Attendance Analysis</h5>
+                      <hr>
+                      <!-- filtering option start -->
+                      <form role="form" data-toggle="validator" action="graphGenerator.php" method="post">
+                          <div class="department-add">
+                              <div class="col-xs-12">
+                                <div class="form-group">
+                                    <div class="col-xs-1">
+                                    </div>
+                                </div>
+
+                                  <div class="form-group">
+                                      <label class="col-xs-2 control-label form-lable">Start date:</label>
+                                      <div class="col-xs-3">
+                                          <input id="example1" name="start_date" type="text"
+                                                 placeholder="Start Date"
+                                                 class="form-control input-md" required>
+                                      </div>
+                                  </div>
+
+                                  <div class="form-group">
+                                    <label class="col-xs-2 control-label form-lable">End date:</label>
+                                      <div class="col-xs-3">
+                                          <input id="example2" name="end_date" type="text"
+                                                 placeholder="End Date"
+                                                 class="form-control input-md" required>
+
+                                      </div>
+                                  </div>
+
+                                  <div class="col-xs-2">
+                                    <button class="btn btn-primary btn-lg pull-right submit-button" style="width: 150px " type="submit">Fitler</button>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="col-xs-1">
+                                    </div>
+                                </div>
+                              </div>
+                          </div>
+                      </form>
+                    <!-- filtering option end -->
+                    <br><br><br><br><br><br>
+
+  <?php
         //take the dates within the range
         $drange=array();
         $empid=array();
@@ -82,7 +248,7 @@ $d2=strtotime($endDate);
                 $j=$j+1;
               }
             }
-            //print_r($emp);
+            //print_r($dte);
             $dCount=(array_count_values($dte));
 
             //take employee count in that department
